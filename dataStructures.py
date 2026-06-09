ee957f236d74a4e58ec1579a939e2dcb1e075dd2
stivenher <stivenher99@gmail.com>
2026-06-09 14:33:45 -0500

    Add first .py

---
+++ dataStructures.py
@@ -0,0 +1,22 @@
+#listas:
+listA=[1,"2",True,5.6]
+listB=(list(range(2,5))) # "list" is type modifier for range object
+print()
+print (listA,type(listA), sep=" , ", end=" que mas esperas?\n")
+print (listB,type(listB), sep=" , ", end=" bruh .|.\n")
+
+#tuplas:
+tupleA=(1,"2",False,5.6)
+print(tupleA,type(tupleA),sep=" , ")
+
+#Diccionario
+dictA={"a":1,
+       "b":2,
+       "c":3}
+print(dictA["b"])
+
+#Conjuntos
+setA=set([1,2,3,4]) # "set" is type modifier for the list
+print(setA,type(setA), sep=" , ")
+
+
